0e289c52def2e891773b91f864c17e22eabb1b3d
Shawn Thompson <plansmash@me.com>
2015-06-02 18:52:00 -0400

    added menu function

--- functions.php
+++ functions.php
@@ -38,4 +38,12 @@
     	));
     }
 
+    if (function_exists('register_nav_menus')) {
+        register_nav_menus(
+            array(
+                'main_nav' => 'Main navigation menu'
+                )
+        );
+    }
+
 ?>
