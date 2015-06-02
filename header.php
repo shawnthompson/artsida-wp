96a72dfb3f8a8d9f4ce7f4b480817c5d3e3391a1
Shawn Thompson <plansmash@me.com>
2015-06-02 10:25:08 -0400

    Updated styles

--- header.php
+++ header.php
@@ -6,16 +6,13 @@
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
 	<?php if (is_search()) { ?>
 	   <meta name="robots" content="noindex, nofollow" />
 	<?php } ?>
-
 	<title>
 		   <?php
 			  if (function_exists('is_tag') && is_tag()) {
@@ -36,29 +33,23 @@
 				 echo ' - page '. $paged; }
 		   ?>
 	</title>
-
 	<link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicon.ico"  type="image/x-icon">
-	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/bootstrap.min.css">
-	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">-->
-	<!-- Font Awesome -->
-	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/font-awesome.min.css">
-	<!--<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">-->
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<!--[if gte IE 9 | !IE ]><!-->
+		<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/theme.min.css">
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	<![endif]-->
 	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/theme.ie.css" />
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
 	<?php if ( is_singular() ) wp_enqueue_script('comment-reply'); ?>
-
 	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
-
 	<header class="relative">
 		<div class="col-sm-8">
 			<div class="row">
