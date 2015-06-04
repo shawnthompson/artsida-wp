29a31d87076a143f0ed878b9dccd1e3db362b95b
Shawn Thompson <plansmash@me.com>
2015-06-04 12:33:20 -0400

    Optimized the header and page templates

--- page.php
+++ page.php
@@ -4,21 +4,6 @@
 
 
 
-		<div class="cell">
-			<div class="col-sm-2 hidden-xs pink"></div>
-			<div class="col-sm-3 hidden-xs cyan"></div>
-			<div class="col-sm-7 purple"><h1><?php the_title(); ?></h1></div>
-		</div>
-		<div class="col-sm-12 text">
-			<div class="col-sm-4 hidden-xs">
-			<?php
-				if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-				    add_filter( 'post_thumbnail_html', 'remove_img_attr' ); // removes the size attributes
-					the_post_thumbnail('full', array('class' => 'img-responsive' ));
-				}
-			?>
-			</div>
-			<div class="col-sm-6">
 
 			<?php the_content(); ?>
 
