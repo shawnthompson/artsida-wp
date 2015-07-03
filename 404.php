9e6309a53f1cb429bccf53a66edaf696518371a0
Shawn Thompson <plansmash@me.com>
2015-07-02 22:04:18 -0400

    created 404 page

--- 404.php
+++ 404.php
@@ -1,6 +1,13 @@
 <?php get_header(); ?>
-
-	<h2>Error 404 - Page Not Found</h2>
-
-
+		<header class="cell purple">
+			<div class="col-sm-2 hidden-xs pink"></div>
+			<div class="col-sm-3 hidden-xs cyan"></div>
+			<div class="col-sm-7 purple">
+				<h1>Error 404 - Page Not Found</h1>
+			</div>
+			<div class="clearfix"></div>
+		</header>
+		<div class="col-sm-12 text">
+			<p class="text-center"><?php _e('[:en]Page not find. Please visit the <a href="'. get_option('home'). '/?p=16">artwork</a> or use the menu to navigate to the right page.[:][:fr]Cette page ne trouvez pas. S\'il vous plaît visiter <a href="'. get_option('home') . '/?p=16">l\'œuvre d\'art</a> ou utiliser le menu pour accéder à la bonne page.[:]'); ?></p>
+		</div>
 <?php get_footer(); ?>
