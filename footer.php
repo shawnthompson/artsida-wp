bd9fe5d4bda9a93d1a0cdd72e86190bd19ed8eb7
Shawn Thompson <plansmash@me.com>
2017-01-04 20:26:56 -0500

    update sponsors in footer

--- footer.php
+++ footer.php
@@ -24,30 +24,33 @@
 			<h3><?php _e('[:en]Framing Sponsor[:][:fr]Commanditaire Encadrement[:]'); ?>:</h3>
 				<a target="_blank" href="http://www.encadrex.com/index.php/<?php _e('[:en]en-US/[:][:fr]fr/[:]'); ?>"><img class="img-responsive mrgn-tp-sm" src="<?php bloginfo('template_url'); ?>/img/sponsor-encadrex.png" alt="encadrex"></a>
 		</section>
-		<div class="col-xs-8 row">
-			<div class="col-xs-4 vertical-center">
+		<div class="col-xs-8 row mrgn-bttm-lg">
+			<div class="col-xs-4">
 				<a target="_blank" href="http://www.eskawater.com/?lang=<?php _e('[:en]en[:][:fr]fr[:]'); ?>"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-eska.png" alt=""></a>
 			</div>
-			<div class="col-xs-4 vertical-center">
+			<div class="col-xs-4">
 				<a target="_blank" href="<?php _e('[:en]http://image-24.com/[:][:fr]http://image-24.com/fr/[:]'); ?>"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-image24.png" alt=""></a>
 			</div>
-			<div class="col-xs-4 vertical-center">
+			<div class="col-xs-4">
 				<a target="_blank" href="http://maysys.com/"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-maysys.png" alt=""></a>
 			</div>
 		</div>
-		<div class="col-xs-8 row sponsor">
-			<div class="col-xs-2 vertical-center">
-				<a target="_blank" href="https://www.matelasbonheur.ca/<?php _e('[:en]en[:][:fr]fr[:]'); ?>"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-matelas.png" alt=""></a>
-			</div>
-			<div class="col-xs-2 vertical-center">
-				<a target="_blank" href="http://pamco.ca/"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-pamco.jpg" alt=""></a>
-			</div>
-			<div class="col-xs-2 vertical-center">
-				<a target="_blank" href="<?php _e('[:en]http://codespark.ca/en/[:][:fr]http://codespark.ca/[:]'); ?>"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-CodeSpark.png" alt=""></a>
-			</div>
-			<div class="col-xs-2 vertical-center">
-				<a target="_blank" href="<?php _e('[:en]http://lmkca.com/en/[:][:fr]http://lmkca.com[:]'); ?>"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-lacharite.png" alt=""></a>
-			</div>
+			<div class="col-xs-8 text-center row sponsor">
+				<div class="col-xs-4">
+					<a target="_blank" href="https://www.matelasbonheur.ca/<?php _e('[:en]en[:][:fr]fr[:]'); ?>"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-matelas.png" alt=""></a>
+				</div>
+				<div class="col-xs-4">
+					<a target="_blank" href="http://pamco.ca/"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-pamco.jpg" alt=""></a>
+				</div>
+				<div class="col-xs-4">
+					<a target="_blank" href="<?php _e('[:en]http://codespark.ca/en/[:][:fr]http://codespark.ca/[:]'); ?>"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-CodeSpark.png" alt=""></a>
+				</div>
+				<div class="col-xs-4">
+					<a target="_blank" href="<?php _e('[:en]http://lmkca.com/en/[:][:fr]http://lmkca.com[:]'); ?>"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-lacharite.png" alt=""></a>
+				</div>
+				<div class="col-xs-4">
+					<a target="_blank" href="<?php _e('[:en]http://theratech.com[:][:fr]http://theratech.com/fr[:]'); ?>"><img class="img-responsive" src="<?php bloginfo('template_url'); ?>/img/sponsor-thera.png" alt=""></a>
+				</div>
 		</div>
 	</footer>
 	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
