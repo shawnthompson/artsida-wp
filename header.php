abfb0b376876c2f6fe05a7a6bb62232004101fcd
Shawn Thompson <plansmash@me.com>
2018-12-06 18:03:14 -0500

    removed ticket page styles

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 	$remaining = $date - time();
 	$days = floor($remaining / 86400);
 	$hours = floor(($remaining % 86400) / 3600);
-	$cssVersion = "2.0.1";
+	$cssVersion = "2.0.2";
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
